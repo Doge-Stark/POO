28f7e78f486f21fa47fd62b2f683daae0afd2cb5
Pedro Inácio <pedro.i09@aluno.ifsc.edu.br>
2026-08-10 12:43:55 -0300

    começo do lab-02 exercício 05

---
+++ laboratorios/lab-02/leituraArquivos/src/main/java/engtelecom/poo/Main.java
@@ -0,0 +1,30 @@
+package engtelecom.poo;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+
+        char[][] chars = new char[9][9];
+        Scanner leitor = new Scanner(System.in);
+
+        int aux = 0;
+
+        while(leitor.hasNext()){
+            String linha = leitor.nextLine();
+            char dados[] =  linha.toCharArray();
+
+            for (int j = 0; j < 9 && j < dados.length; j++) {
+                chars[aux][j] = dados[j];
+            }
+            aux++;
+        }
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+               IO.print(chars[i][j]);
+            }
+        }
+        leitor.close();
+    }
+}
